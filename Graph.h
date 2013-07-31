b2e43f339b3709cc4c0d3dc13c96e3bbbe675ac7
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-07-30 23:05:22 -0500

    adding working framework

---
+++ Graph.h
@@ -0,0 +1,277 @@
+// ----------------------
+// projects/graph/Graph.h
+// Copyright (C) 2013
+// Glenn P. Downing
+// ----------------------
+
+#ifndef Graph_h
+#define Graph_h
+
+// --------
+// includes
+// --------
+
+#include <cassert> // assert
+#include <cstddef> // size_t
+#include <utility> // make_pair, pair
+#include <vector>  // vector
+#include <list>
+#include <deque>
+
+// -----
+// Graph
+// -----
+// template <typename V = int, typename VC = std::set<V>, typename E = int, typename EC = std::deque<E>, bool DIRECTED = true>
+class Graph {
+    public:
+        // --------
+        // typedefs
+        // --------
+
+        typedef int vertex_descriptor;  // fix!
+        typedef std::pair<vertex_descriptor, vertex_descriptor> edge_descriptor;    // fix!
+
+        typedef std::vector<vertex_descriptor>::const_iterator vertex_iterator;    // fix!
+        typedef std::vector<edge_descriptor>::const_iterator edge_iterator;      // fix!
+        typedef std::set<vertex_descriptor>::const_iterator adjacency_iterator; // fix!
+
+        // typedef typename VC::const_iterator const_vertex_iterator;    // fix!
+        // typedef typename EC::const_iterator const_edge_iterator;      // fix!
+        // typedef typename VC::const_iterator const_adjacency_iterator; // fix!
+
+        typedef std::size_t vertices_size_type;
+        typedef std::size_t edges_size_type;
+
+    public:
+        // --------
+        // add_edge
+        // --------
+
+        /**
+         * <your documentation>
+         */
+        friend std::pair<edge_descriptor, bool> add_edge (vertex_descriptor u, vertex_descriptor v, Graph& g) {
+            // <your code>
+            edge_descriptor ed = std::make_pair(u, v);
+            // bool            b;
+
+            // ae is a pair returning the iterator of the inserted vertext v2 and bool whether v2 was inserted
+            std::pair<adjacency_iterator,bool> ae = g._g[u].insert(v);
+            if(ae.second == true) 
+                g._ec.push_back(ed);
+            return std::make_pair(ed, ae.second);}
+
+        // ----------
+        // add_vertex
+        // ----------
+
+        /**
+         * <your documentation> DONE
+         add a vertex v to graph g 
+         */
+        friend vertex_descriptor add_vertex (Graph& g) {
+            // <your code> DONE 
+            vertex_descriptor v = g._g.size();
+            g._vc.push_back(v);
+            g._g.push_back(std::set<vertex_descriptor>());
+            return v;}
+
+        // -----------------
+        // adjacent_vertices
+        // -----------------
+
+        /**
+         * <your documentation> DONE
+         returns first and last+1 iterators for vertices adjacent to vertex v in graph g
+         */
+        friend std::pair<adjacency_iterator, adjacency_iterator> adjacent_vertices (vertex_descriptor v, const Graph& g) {
+            // <your code> DONE
+
+            adjacency_iterator b = g._g[v].begin();
+            adjacency_iterator e = g._g[v].end();
+            return std::make_pair(b, e);}
+
+        // ----
+        // edge
+        // ----
+
+        /**
+         * <your documentation> DONE
+         returns the edge_descriptor for the given vertices u & v in graph g and if the edge exists
+         */
+        friend std::pair<edge_descriptor, bool> edge (vertex_descriptor u, vertex_descriptor v, const Graph& g) {
+            // <your code> DONE
+            edge_descriptor ed = std::make_pair(u, v);
+            // bool            b;
+
+            // find(g._ec.begin(), g._ec.end(), v) != g._ec.end();
+            return std::make_pair(ed, find(g._ec.begin(), g._ec.end(), ed) != g._ec.end());}
+
+        // -----
+        // edges
+        // -----
+
+        /**
+         * <your documentation> DONE
+         returns first and last+1 iterators to edges in graph g
+         */
+        friend std::pair<edge_iterator, edge_iterator> edges (const Graph& g) {
+            // <your code> DONE
+            edge_iterator b = g._ec.begin();
+            edge_iterator e = g._ec.end();
+            return std::make_pair(b, e);}
+
+        // ---------
+        // num_edges
+        // ---------
+
+        /**
+         * <your documentation> DONE
+         returns the number of edges in graph g
+         */
+        friend edges_size_type num_edges (const Graph& g) {
+            // <your code> DONE
+            // edges_size_type s;
+            return g._ec.size();}
+
+        // ------------
+        // num_vertices
+        // ------------
+
+        /**
+         * <your documentation> DONE
+         returns the number of vertices in graph g
+         */
+        friend vertices_size_type num_vertices (const Graph& g) {
+            // <your code> DONE
+            // vertices_size_type s;
+            return g._vc.size();}
+
+        // ------
+        // source
+        // ------
+
+        /**
+         * <your documentation> DONE
+         returns the source vertex of edge ed
+         */
+        friend vertex_descriptor source (edge_descriptor ed, const Graph& g) {
+            // <your code> DONE
+            // vertex_descriptor v;
+            return ed.first;}
+
+        // ------
+        // target
+        // ------
+
+        /**
+         * <your documentation> DONE
+         returns the target vertex of edge ed
+         */
+        friend vertex_descriptor target (edge_descriptor ed, const Graph& g) {
+            // <your code> DONE
+            // vertex_descriptor v;
+            return ed.second;}
+
+        // ------
+        // vertex
+        // ------
+
+        /**
+         * <your documentation> DONE
+         returns the vertex_descriptor corresponding to s in graph g
+         */
+        friend vertex_descriptor vertex (vertices_size_type s, const Graph& g) {
+            // <your code> DONE
+            // vertex_descriptor vd;
+            return g._vc[s];}
+
+        // --------
+        // vertices
+        // --------
+
+        /**
+         * <your documentation> DONE
+         return first and last+1 iterators for vertices in graph g
+         */
+        friend std::pair<vertex_iterator, vertex_iterator> vertices (const Graph& g) {
+            // <your code> DONE
+            vertex_iterator b = g._vc.begin();
+            vertex_iterator e = g._vc.end();
+            return std::make_pair(b, e);}
+
+    private:
+        // ----
+        // data
+        // ----
+
+        std::vector<std::set<vertex_descriptor> > _g; // vector of vertices containing set of adjacent vertices
+        std::vector<vertex_descriptor> _vc;  // vector of  vertices for a graph
+        std::vector<edge_descriptor> _ec; //  vector of edges for the graph
+
+        // -----
+        // valid
+        // -----
+
+        /**
+         * <your documentation>
+         */
+        bool valid () const {
+            // <your code>
+            return true;}
+
+    public:
+        // ------------
+        // constructors
+        // ------------
+
+        /**
+         * <your documentation>
+         */
+        Graph (std::vector<std::set<vertex_descriptor> > g = std::vector<std::set<vertex_descriptor> >(), std::vector<vertex_descriptor> vc = std::vector<vertex_descriptor>(), std::vector<edge_descriptor> ec = std::vector<edge_descriptor> ()) :
+                _g(g),
+                _vc(vc),
+                _ec(ec)
+            {
+            // <your code>
+            assert(valid());}
+
+        // Default copy, destructor, and copy assignment
+        // Graph  (const Graph<T>&);
+        // ~Graph ();
+        // Graph& operator = (const Graph&);
+    };
+
+// ---------
+// has_cycle
+// ---------
+
+/**
+ * depth-first traversal
+ * three colors
+ * <your documentation>
+ */
+template <typename G>
+bool has_cycle (const G& g) {
+    return true;}
+
+// ----------------
+// topological_sort
+// ----------------
+
+/**
+ * depth-first traversal
+ * two colors
+ * <your documentation>
+ * @throws Boost's not_a_dag exception if has_cycle()
+ */
+template <typename G, typename OI>
+void topological_sort (const G& g, OI x) {
+    *x = 2;
+    ++x;
+    *x = 0;
+    ++x;
+    *x = 1;
+    }
+
+#endif // Graph_h
---
+++ TestGraph.c++
@@ -0,0 +1,192 @@
+// ----------------------------
+// projects/deque/TestDeque.c++
+// Copyright (C) 2013
+// Glenn P. Downing
+// ----------------------------
+
+/*
+ * TestDeque
+ *
+ * To compile this, use the command
+ * g++ -pedantic -std=c++0x -Wall TestDeque.c++ -o TestDeque -lgtest -lgtest_main -lpthread
+ *
+ * Then it can run with
+ * TestDeque
+ *
+ * It will work on any machine with gtest and the precompiled libraries installed
+ */
+
+// --------
+// includes
+// --------
+
+#include <algorithm> // equal
+#include <cstring>   // strcmp
+#include <sstream>   // ostringstream
+#include <stdexcept> // invalid_argument
+#include <string>    // ==
+
+#include "gtest/gtest.h"                        // Google Test framework
+
+#include "boost/graph/adjacency_list.hpp"  // adjacency_list
+#include "boost/graph/topological_sort.hpp"// topological_sort
+
+#include "Graph.h"
+
+// #include <cassert>
+// #include <iterator>
+// #include <memory>
+
+#include <utility>
+#include <cstdlib>
+
+
+#define class struct
+#define protected public
+#define private public
+
+using namespace boost;
+// ---------
+// TestDeque
+// ---------
+
+typedef testing::Types< adjacency_list<setS, vecS, directedS>, Graph<> > Containers;
+
+// TYPED_TEST_CASE(DequeTest, MyDeques);
+template <typename C>
+class Tests : public testing::Test {
+    typedef C graph_type;
+    typedef typename graph_type::vertex_descriptor  vertex_descriptor;
+    typedef typename graph_type::edge_descriptor    edge_descriptor;
+
+    typedef typename graph_type::vertex_iterator    vertex_iterator;
+    typedef typename graph_type::edge_iterator      edge_iterator;
+    typedef typename graph_type::adjacency_iterator adjacency_iterator;
+
+    typedef typename graph_type::vertices_size_type vertices_size_type;
+    typedef typename graph_type::edges_size_type    edges_size_type;
+    public:
+    graph_type _g;
+    vertex_descriptor _vdA;
+    vertex_descriptor _vdB;
+    vertex_descriptor _vdC;
+    vertex_descriptor _vdD;
+    vertex_descriptor _vdE;
+    vertex_descriptor _vdF;
+    vertex_descriptor _vdG;
+    vertex_descriptor _vdH;
+
+    edge_descriptor _edAB;
+    edge_descriptor _edAC;
+    edge_descriptor _edAE;
+    edge_descriptor _edBD;
+    edge_descriptor _edBE;
+    edge_descriptor _edCD;
+    edge_descriptor _edDE;
+    edge_descriptor _edDF;
+    edge_descriptor _edFD;
+    edge_descriptor _edFH;
+    edge_descriptor _edGH;
+
+    Tests():_g(graph_type()),
+        _vdA(add_vertex(_g)),
+        _vdB(add_vertex(_g)),
+        _vdC(add_vertex(_g)),
+        _vdD(add_vertex(_g)),
+        _vdE(add_vertex(_g)),
+        _vdF(add_vertex(_g)),
+        _vdG(add_vertex(_g)),
+        _vdH(add_vertex(_g)),
+        _edAB(add_edge(_vdA, _vdB, _g).first),
+        _edAC(add_edge(_vdA, _vdC, _g).first),
+        _edAE(add_edge(_vdA, _vdE, _g).first),
+        _edBD ( add_edge(_vdB, _vdD, _g).first),
+        _edBE ( add_edge(_vdB, _vdE, _g).first),
+        _edCD ( add_edge(_vdC, _vdD, _g).first),
+        _edDE ( add_edge(_vdD, _vdE, _g).first),
+        _edDF ( add_edge(_vdD, _vdF, _g).first),
+        _edFD ( add_edge(_vdF, _vdD, _g).first),
+        _edFH ( add_edge(_vdF, _vdH, _g).first),
+        _edGH ( add_edge(_vdG, _vdH, _g).first){}
+};
+
+
+
+TYPED_TEST_CASE(Tests, Containers);
+
+TYPED_TEST (Tests, add_edge_1) {
+    std::pair<typename TestFixture::edge_descriptor, bool> p = add_edge(this->_vdA, this->_vdB, this->_g);
+    EXPECT_TRUE(p.first  == this->_edAB);
+    EXPECT_TRUE(p.second == false);}
+
+ TYPED_TEST (Tests, adjacent_vertices_1) {
+        std::pair<typename TestFixture::adjacency_iterator, typename TestFixture::adjacency_iterator> p = adjacent_vertices(this->_vdA, this->_g);
+        typename TestFixture::adjacency_iterator b = p.first;
+        typename TestFixture::adjacency_iterator e = p.second;
+        EXPECT_TRUE(b != e);
+        if (b != e) {
+            typename TestFixture::vertex_descriptor vd = *b;
+            EXPECT_TRUE(vd == this->_vdB);}
+        ++b;
+        if (b != e) {
+            typename TestFixture::vertex_descriptor vd = *b;
+            EXPECT_TRUE(vd == this->_vdC);}}
+
+TYPED_TEST (Tests, test_edge_1) {
+        std::pair<typename TestFixture::edge_descriptor, bool> p = edge(this->_vdA, this->_vdB, this->_g);
+        EXPECT_TRUE(p.first  == this->_edAB);
+        EXPECT_TRUE(p.second == true);}
+
+TYPED_TEST (Tests,test_edges_1) {
+        std::pair<typename TestFixture::edge_iterator, typename TestFixture::edge_iterator> p = edges(this->_g);
+        typename TestFixture::edge_iterator b = p.first;
+        typename TestFixture::edge_iterator e = p.second;
+        EXPECT_TRUE(b != e);
+        if (b != e) {
+            typename TestFixture::edge_descriptor ed = *b;
+            EXPECT_TRUE(ed == this->_edAB);}
+        ++b;
+        if (b != e) {
+            typename TestFixture::edge_descriptor ed = *b;
+            EXPECT_TRUE(ed == this->_edAC);}}
+
+TYPED_TEST (Tests,test_num_edges_1) {
+        typename TestFixture::edges_size_type es = num_edges(this->_g);
+        EXPECT_TRUE(es == 11);}
+
+TYPED_TEST (Tests,test_num_vertices_1) {
+        typename TestFixture::vertices_size_type vs = num_vertices(this->_g);
+        EXPECT_TRUE(vs == 8);}
+
+TYPED_TEST (Tests,test_source_1) {
+        typename TestFixture::vertex_descriptor vd = source(this->_edAB, this->_g);
+        EXPECT_TRUE(vd == this->_vdA);}
+
+TYPED_TEST (Tests,test_target_1) {
+        typename TestFixture::vertex_descriptor vd = target(this->_edAB, this->_g);
+        EXPECT_TRUE(vd == this->_vdB);}
+
+TYPED_TEST (Tests,test_vertex_1) {
+        typename TestFixture::vertex_descriptor vd = vertex(0, this->_g);
+        EXPECT_TRUE(vd == this->_vdA);}
+
+TYPED_TEST (Tests,test_vertices_1) {
+        std::pair<typename TestFixture::vertex_iterator, typename TestFixture::vertex_iterator> p = vertices(this->_g);
+        typename TestFixture::vertex_iterator b = p.first;
+        typename TestFixture::vertex_iterator e = p.second;
+        EXPECT_TRUE(b != e);
+        if (b != e) {
+            typename TestFixture::vertex_descriptor vd = *b;
+            EXPECT_TRUE(vd == this->_vdA);}
+        ++b;
+        if (b != e) {
+            typename TestFixture::vertex_descriptor vd = *b;
+            EXPECT_TRUE(vd == this->_vdB);}}
+
+// TEST (Tests,test_has_cycle_1) {
+//         EXPECT_TRUE(has_cycle(this->_g));}
+
+// TEST (Tests,test_topological_sort_1) {
+//         std::ostringstream out;
+//         topological_sort(this->_g, std::ostream_iterator<vertex_descriptor>(out, " "));
+//         EXPECT_TRUE(out.str() == "2 0 1 ");}
